42c9b969a360773addf7fb9bf79262698a48691e
LeandritKrasniqi <leandritkrasniqi1@gmail.com>
2021-09-02 17:03:46 +0200

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+
+    public void addThreeNumbers(int num1, int num2, int num3){
+        System.out.println(num1*num2*num3);
+
+    }
+}
+
+
+
+
---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.addThreeNumbers(4, 4, 4);
+    }
+}
